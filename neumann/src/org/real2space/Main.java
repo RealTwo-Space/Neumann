9c08d724b7d9dd6cf55b7f848b6a4c77a327377d
RyosukeCla <ryosukeclarinet@gmail.com>
2016-10-10 13:38:45 +0900

    Construct Environment
    - add .utility
    - add approssi, bravoplot, congraph
    - add test template

--- neumann/src/org/real2space/Main.java
+++
@@ -1,8 +0,0 @@
-package org.real2space;
-
-public class Main {
-
-    public static void main(String[] args) {
-	    // write your code here
-    }
-}
---
+++ neumann/test/org/real2space/bravoplot/MainTest.java
@@ -0,0 +1,35 @@
+package org.real2space.bravoplot;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project Neumann
+ *
+ * @author RealTwo-Space
+ * @version 0
+ * @created 2016/10/10
+ */
+public class MainTest {
+    private Main test;
+    @Before
+    public void setUp() throws Exception {
+        this.test = new Main();
+        this.test.test();
+        System.out.println ("bravoplot test");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void test1() throws Exception {
+
+    }
+
+}
