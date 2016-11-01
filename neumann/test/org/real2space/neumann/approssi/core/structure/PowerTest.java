56d5771bcb1f0ca1eb24465c8bf1ce740424c1f8
RyosukeCla <ryosukeclarinet@gmail.com>
2016-11-01 15:02:14 +0900

    add core package in approssi

---
+++ neumann/test/org/real2space/neumann/approssi/core/structure/PowerTest.java
@@ -0,0 +1,14 @@
+package org.real2space.neumann.approssi.core.structure;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project Neumann
+ *
+ * @author RealTwo-Space
+ * @version 0
+ *          created     2016/11/01
+ */
+public class PowerTest {
+
+}
