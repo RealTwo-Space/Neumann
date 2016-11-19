99d788fee3cddc665134e1de65cca2f6fa5285ea
Ryu Fujiwara <ev50061@Ryu-Fujiwaranomac-book-air.local>
2016-11-20 00:32:07 +0900

    Created test approssi/value

---
+++ neumann/test/org/real2space/neumann/approssi/core/value/MatrixC64Test.java
@@ -0,0 +1,15 @@
+package org.real2space.neumann.approssi.core.value;
+
+import static org.junit.Assert.*;
+
+/**
+ * Project Neumann
+ *
+ * @author RealTwo-Space
+ * @version 0
+ *          <p>
+ *          created 11/20/16
+ */
+public class MatrixC64Test {
+
+}
