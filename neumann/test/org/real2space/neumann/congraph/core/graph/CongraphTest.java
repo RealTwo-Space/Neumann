bb8ca1f1020db1de65cb779472264b15617535c8
Ryu Fujiwara <ev50061@Ryu-Fujiwaranomac-book-air.local>
2016-12-24 02:40:32 +0900

    Added Golden Ratio tutorial

--- neumann/test/org/real2space/neumann/congraph/core/graph/CongraphTest.java
+++ neumann/test/org/real2space/neumann/congraph/core/graph/CongraphTest.java
@@ -29,8 +29,54 @@ public class CongraphTest {
         Node mulB = cg.multiply(va, d);
         Node sub = cg.substitute(va, mulB);
 
+        /*
         for (int i = 0; i < 10; i++) {
             cg.execute(sub);
+        }
+        */
+
+        Node n1 = cg.doubleVariable(0.0);
+        Node n2 = cg.doubleConstant(1.0);
+        Node n3 = cg.add(n1, n2);
+        Node n4 = cg.substitute(n1, n3);
+        Node n5 = cg.matrixConstant(m);
+        Node n6 = cg.multiply(n5, n4);
+
+        /*
+        for (int i = 0; i < 10; i++) {
+            cg.execute(n6);
+        }
+        */
+
+
+
+    }
+
+    @Test
+    public void goldenRatioTest () {
+        Congraph cg = new Congraph();
+
+        Node ren1 = cg.doubleVariable(1.0); // n - 1
+        Node ren2 = cg.doubleVariable(1.0); // n
+        Node ren3 = cg.add(ren1, ren2);
+        Node ren4 = cg.add(ren3, ren2);
+        Node ren5 = cg.substitute(ren1, ren3);
+        Node ren6 = cg.substitute(ren2, ren4);
+        Node ren7 = cg.divide(ren6, ren5);
+
+
+        /*
+        Node ren1 = cg.floatVariable(1.0f); // n - 1
+        Node ren2 = cg.floatVariable(1.0f); // n
+        Node ren3 = cg.add(ren1, ren2);
+        Node ren4 = cg.add(ren3, ren2);
+        Node ren5 = cg.substitute(ren1, ren3);
+        Node ren6 = cg.substitute(ren2, ren4);
+        Node ren7 = cg.divide(ren6, ren5);
+        */
+
+        for (int i = 0; i < 20; i++) {
+            cg.execute(ren7);
         }
     }
 
